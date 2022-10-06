1244c52a657cbd2236ed1679b3b0fe6bef728344
Rebeca Oliveira <r.soares.oliveira@avanade.com>
2022-10-06 10:08:04 -0700

    New

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,20 +1,45 @@
 const cep = document.querySelector("#cep");
 
-cep.addEventListener("blur",(e)=>{
-    let search = cep.value.replace("-", "");
-
-    if(search.length  < 8 ){
-        alert("CEP inválido!");
-        cep.focus();
-        return;
+const showData = (result) => {
+  for (const campo in result) {
+    if (document.querySelector("#" + campo)) {
+      document.querySelector("#" + campo).value = result[campo];
     }
+  }
+};
 
-    const options = {
-        method:"GET",
-        mode: "cors",
-        cache:"default"
-    }
-    //recurso assíncrono retorna uma promise por que eu não sei se vai funcionar.
-    fetch (`https://viacep.com.br/ws/${search}/json`, options)
+cep.addEventListener("blur", async (e) => {
+  let search = cep.value.replace("-", "");
+  if (search.length < 8 || Number(search.value) === NaN) {
+    alert("CEP inválido!");
+    cep.focus();
+    return;
+  }
+  const options = {
+    method: "GET",
+    mode: "cors",
+    cache: "default",
+  };
+  // axios - biblioteca javascript que usa fetch e XHR.
+  try {
+    const resultado = await fetch(
+      `https://viacep.com.br/ws/${search}/json/`,
+      options
+    );
+    const json = await resultado.json();
+    showData(json);
+  } catch (error) {
+    console.log("Temos um problemas", error.message);
+  }
+});
 
-    });
+//   fetch(`https://viacep.com.br/ws/${search}/json/`, options)
+//     .then((response) => {
+//       response.json().then((result) => {
+//         console.log(result);
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(e.message);
+//     });
+
